5d45df80a9b3eb9bd6fd4d508e0c3f04aab8655a
Mon86 <hugomon.1901@gmail.com>
2022-08-29 23:31:17 -0300

    desafio

---
+++ script.js
@@ -0,0 +1,26 @@
+// let nombre= prompt ("ingrese su nombre");
+// let pass = prompt ("ingrese su pass");
+
+// for (let i = 2; i >=0 ; i--){
+//     let contra= prompt("ingrese nuevamente la contrasena");
+//     if (pass== contra) {alert ("BIENVENIDO "+ nombre);
+//     break;
+//     }
+//     else if (i== 0) {alert ("su cuenta fue bloqueada");
+// }
+// else {alert ("SE EQUIVOCO, LE QUEDAN" + i + "intentos");
+// }
+// }
+
+
+
+// WHILE
+
+pelicula= ( " ")
+
+do { 
+    let pelicula= prompt ("ingrese su pelicula, o pulse ESC para salir");
+    alert ("buscando la pelicula " + pelicula );
+    
+    
+} while (pelicula == "ESC");
